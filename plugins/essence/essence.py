71737bedf21b2eeef222bf0c15c0ce650878dd4e
Zecyel <zecyel@163.com>
2024-03-07 15:35:47 +0800

    feat: allow fumo to mark a message as essence.

---
+++ plugins/essence/essence.py
@@ -0,0 +1,14 @@
+from sdk.send_message import send_group_message
+from sdk.message import text_message
+from core.plugin import Plugin
+from sdk.manage import set_essence
+
+async def handler(session: str, group_id: int, sender_id: int, message):
+    await set_essence(session, message[0]["message_id"], group_id)
+    await send_group_message(session, group_id, text_message("已设精"))
+
+def checker(group_id: int, sender_id: int, message):
+    return message[1] == '设精'
+
+essence = Plugin('essence')
+essence.register_callback('group.QP', handler, checker)
