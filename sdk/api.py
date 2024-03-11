f9ea94f05db97571f738708a5069bc018067c390
Zecyel <zecyel@163.com>
2024-03-11 19:45:57 +0800

    feat: enabled remote test.

--- sdk/api.py
+++ sdk/api.py
@@ -1,17 +1,15 @@
 import requests, json
-from config import IP, PORT
-
-prefix = f'http://{IP}:{PORT}'
+from config import URL_PREFIX
 
 def post(url, body):
-    ret = requests.post(prefix + url, data=json.dumps(body)).json()
+    ret = requests.post(URL_PREFIX + url, data=json.dumps(body)).json()
     if "code" in ret and ret["code"] != 0:
         print("Operation failed.") # future will be log
     return ret
 
 
 def get(url):
-    ret = requests.get(prefix + url).json()
+    ret = requests.get(URL_PREFIX + url).json()
     if "code" in ret and ret["code"] != 0:
         print("Operation failed.")
     return ret
