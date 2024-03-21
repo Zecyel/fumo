213e65d9a0d785e659de246eb9a336170f1be162
Zecyel <zecyel@163.com>
2024-03-21 18:19:47 +0800

    refactor: rewwrite choosing_helper.

--- plugins/choosing_helper/choosing_helper.py
+++ plugins/choosing_helper/choosing_helper.py
@@ -4,16 +4,21 @@ from core.plugin import Plugin
 import random
 
 async def handler(session: str,group_id: int,sender_id: int,message):
-    if message[1][0]=='我':
-        message[1]=message[1][1:]
-    list = message[1].split('还是')
-    list.append('杂鱼～我才不回答你的问题呢')
-    if list[0]==list[1]:
+    message = message[1]
+    if message.startswith("我"):
+        message = message[1:]
+    choice = message.split('还是')
+    if all([i == choice[0] for i in choice]):
         await send_group_message(session, group_id, text_message('杂鱼～你还想骗我说你想要听的话？'))
-    elif ('是' in list[0]) and (list[0].split('是')[0] == list[1].split('是')[1]):
+        return
+
+    if len(choice) == 2 and set(choice[0].split("是")) == set(choice[1].split("是")):
         await send_group_message(session, group_id, text_message('杂鱼～你还想骗我说你想要听的话？'))
-    else:
-        await send_group_message(session, group_id, text_message(random.choice(list)))
+        return
+
+    choice.append('杂鱼～我才不回答你的问题呢')
+    await send_group_message(session, group_id, text_message(random.choice(choice)))
+
 def checker(group_id: int,sender_id: int,message):
     return '还是' in message[1]
 
