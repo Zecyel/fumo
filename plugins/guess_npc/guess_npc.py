775730597f27ba656771bab669923585ba0ba3be
Zecyel <zecyel@163.com>
2024-03-28 15:47:40 +0800

    feat: add new question bank to guess_npc.

--- plugins/guess_npc/guess_npc.py
+++ plugins/guess_npc/guess_npc.py
@@ -6,7 +6,11 @@ from core.plugin import Plugin
 from sdk.temp_data import alloc, fetch, dump
 from sdk.user import user_group_nickname
 
-npc_name = [key for (key, value) in npc_saying.items()]
+# npc_name = [key for (key, value) in npc_saying.items()]
+npc_name = []
+
+for mod in npc_saying.values():
+    npc_name = [*npc_name, *list(mod.keys())]
 
 async def handler(session: str, group_id: int, sender_id: int, message):
     key = f"guess_npc_{group_id}"
@@ -31,14 +35,31 @@ def checker(group_id: int, sender_id: int, message):
     return message[0] == "结束游戏" or message[0] in npc_name
 
 async def startup_handler(session: str, group_id: int, sender_id: int, message):
+    message = message[1][4:].strip()
+    if message == "":
+        message = "原版"
+    
+    mods = message.split('+')
+    sayings = {}
+
+    for mod in mods:
+        if mod not in npc_saying:
+            await send_group_message(session, group_id, text_message("找不到对应的语录哦~"))
+            return
+        for (key, value) in npc_saying[mod].items():
+            if key not in sayings:
+                sayings[key] = value
+            else:
+                sayings[key] = [*sayings[key], *value]
+
     key = f"guess_npc_{group_id}"
 
     if fetch(key) != None:
         await send_group_message(session, group_id, text_message("请先猜对当前npc，或输入“结束游戏”"))
         return
     
-    npc = random.choice(npc_name)
-    saying = random.choice(npc_saying[npc])
+    npc = random.choice(list(sayings.keys()))
+    saying = random.choice(sayings[npc])
     saying.replace("__name__", user_group_nickname(session, group_id, sender_id))
     alloc(key, {
         "npc": npc,
@@ -48,7 +69,7 @@ async def startup_handler(session: str, group_id: int, sender_id: int, message):
 
 
 def startup_checker(group_id: int, sender_id: int, message):
-    return message[1] == "猜npc"
+    return message[1].startswith("猜npc")
 
 guess_npc = Plugin('guess_npc')
 guess_npc.register_callback('group.@fumoP', startup_handler, startup_checker)
